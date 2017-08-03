e18e76465c31e96e78d4dd4a102801400391535d
Tyler Fridley <email": null>
2017-08-03 16:06:34 -0500

    Add user model/migration and remove auth link from index

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+
+  validates_presence_of :username, :email, :password
+  validates_uniqueness_of :username, :email
+  validates_format_of :email, with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+end
