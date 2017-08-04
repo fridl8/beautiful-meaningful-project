88a684ea8d231e2e026dc553e4328bc5bc62168a
Tyler Fridley <email": null>
2017-08-04 13:40:31 -0500

    Change controller and view for event to add new events to calendar

--- app/controllers/calendar.rb
+++ app/controllers/calendar.rb
@@ -1,6 +1,13 @@
 get '/users/:id/calendar' do
   @user = User.find_by(id: params[:id])
-  if params[:auth_token]
+  if @user.refresh.length != nil
+    @cal = Google::Calendar.new(:client_id     => ENV['GOOGLE_CLIENT_ID'],
+                           :client_secret => ENV['GOOGLE_CLIENT_SECRET'],
+                           :calendar      => ENV['GOOGLE_CAL_ID'],
+                           :refresh_token => @user.refresh,
+                           :redirect_url  => "urn:ietf:wg:oauth:2.0:oob"
+                           )
+  elsif params[:auth_token]
     @cal = Google::Calendar.new(:client_id     => ENV['GOOGLE_CLIENT_ID'],
                            :client_secret => ENV['GOOGLE_CLIENT_SECRET'],
                            :calendar      => ENV['GOOGLE_CAL_ID'],
@@ -9,13 +16,6 @@ get '/users/:id/calendar' do
     refresh_token = @cal.login_with_auth_code(params[:auth_token])
     @user.refresh = refresh_token
     @user.save
-  elsif @user.refresh.length != nil
-    @cal = Google::Calendar.new(:client_id     => ENV['GOOGLE_CLIENT_ID'],
-                           :client_secret => ENV['GOOGLE_CLIENT_SECRET'],
-                           :calendar      => ENV['GOOGLE_CAL_ID'],
-                           :refresh_token => @user.refresh,
-                           :redirect_url  => "urn:ietf:wg:oauth:2.0:oob"
-                           )
   else params[:refresh_token]
     @cal.login_with_refresh_token(params[:refresh_token])
   end
